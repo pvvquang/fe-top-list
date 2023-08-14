c5cc98c04df7a5eddb8d3b7870afdc3e73f6009a
pvvquang <pvquangbg@gmail.com>
2023-08-14 16:59:17 +0700

    feat: add constants

---
+++ src/constants/path.constant.ts
@@ -0,0 +1,7 @@
+// api
+export const API_LOGIN = "/auth/login";
+export const API_USER_INFO = "/auth/self-info";
+
+// routes
+export const ROUTE_LOGIN = "/login";
+export const ROUTE_ADMIN = "/admin";
