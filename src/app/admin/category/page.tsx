3cd8905ef0cd05bccc1cfd79fb692ce8f066813b
pvvquang <pvquangbg@gmail.com>
2023-08-16 17:33:15 +0700

    feat: update admin category page

--- src/app/admin/category/page.tsx
+++ src/app/admin/category/page.tsx
@@ -1,9 +1,62 @@
 "use client";
 
-import React from "react";
+import Button from "@/components/atoms/buttons";
+import TableBase from "@/components/molecules/tables";
+import { TableColumn } from "@/components/molecules/tables/table.type";
+import ModalAddCategory from "@/components/organisms/modals/ModalAddCategory";
+import { useState } from "react";
 
 function CategoryPage() {
-  return <div>CategoryPage</div>;
+  const [openModalAddCategory, setOpenModalAddCategory] =
+    useState<boolean>(false);
+
+  const columns: TableColumn[] = [
+    {
+      id: "categoryName",
+      label: "Category Name",
+    },
+    {
+      id: "delete",
+      label: "Action",
+      align: "center",
+    },
+  ];
+
+  const rows = [
+    {
+      categoryName: "Viet Name",
+      delete: <Button label="Delete" onClick={() => {}} />,
+    },
+    {
+      categoryName: "News",
+      delete: <Button label="Delete" onClick={() => {}} />,
+    },
+    {
+      categoryName: "Trending",
+      delete: <Button label="Delete" onClick={() => {}} />,
+    },
+  ];
+
+  return (
+    <div className="px-10 py-12 max-w-[980px]">
+      <div className="flex items-center justify-between pb-4">
+        <h2>List Category</h2>
+        <Button
+          variant="contained"
+          label="Add Category"
+          onClick={() => setOpenModalAddCategory(true)}
+        />
+      </div>
+
+      <TableBase rows={rows} columns={columns} />
+      {openModalAddCategory && (
+        <ModalAddCategory
+          onClose={() => setOpenModalAddCategory(false)}
+          onSubmit={() => {}}
+        />
+      )}
+    </div>
+  );
 }
 
 export default CategoryPage;
