144be2de86748798f927fc70b458abc6e3cb5fc0
pvvquang <pvquangbg@gmail.com>
2023-08-16 17:32:24 +0700

    feat: update modal, add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 text-white bg-grey-800 opacity-60">
+      loading...
+    </div>
+  );
+}
--- src/components/molecules/modal/index.tsx
+++ src/components/molecules/modal/index.tsx
@@ -8,9 +8,9 @@ interface ModalProps {
 
 function Modal({ children }: ModalProps) {
   return (
-    <div className="relative z-10" aria-modal="true">
-      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-      <div className="fixed inset-0 z-10 overflow-y-auto">
+    <div className="fixed z-50 inset-0" aria-modal="true">
+      <div className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      <div className="absolute inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             {children}
