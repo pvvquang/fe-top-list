fabd115d76e7f7ff1998549f8f19c5b3594543e0
pvvquang <pvquangbg@gmail.com>
2023-08-18 11:48:46 +0700

    feat: logic show loading

--- src/context/AdminProvider.tsx
+++ src/context/AdminProvider.tsx
@@ -1,4 +1,6 @@
 "use client";
+import ConditionalRender from "@/components/atoms/ConditionalRender";
+import Loading from "@/components/atoms/Loading";
 import { getUserInfo } from "@/services/auth.service";
 import { HttpCode } from "@/types/api.type";
 import { User } from "@/types/auth.type";
@@ -6,17 +8,19 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 
 interface AdminContextValue {
   userInfo: User | null;
+  fetchWithLoading: (callback: () => Promise<void>) => Promise<void>;
 }
 
 const AdminContext = createContext<AdminContextValue | null>(null);
 
-const useAdminContent = () => {
+export const useAdminContent = () => {
   const value = useContext(AdminContext);
   if (value === null) throw Error("Admin context has not been Provider");
   return value;
 };
 
 export const AdminProvider = ({ children }: { children: React.ReactNode }) => {
+  const [pageLoading, setPageLoading] = useState<boolean>(false);
   const [userInfo, setUserInfo] = useState<User | null>(null);
 
   const fetchUserInfo = async () => {
@@ -30,12 +34,23 @@ export const AdminProvider = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
+  const fetchWithLoading = async (callback: () => Promise<void>) => {
+    setPageLoading(true);
+    callback().finally(() => {
+      setPageLoading(false);
+    });
+  };
+
   useEffect(() => {
     fetchUserInfo();
   }, []);
 
-  const value = { userInfo };
+  const value = { userInfo, fetchWithLoading };
   return (
-    <AdminContext.Provider value={value}>{children}</AdminContext.Provider>
+    <AdminContext.Provider value={value}>
+      <ConditionalRender conditional={pageLoading} fallback={children}>
+        <Loading />
+      </ConditionalRender>
+    </AdminContext.Provider>
   );
 };
